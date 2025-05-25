4febba89b5a6587f5b7dc82b9c55850b1d6005a5
minamirza <minamirzeva@gmail.com>
2025-05-25 17:32:38 +0400

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,19 @@
+import { useState } from 'react'
+import { Routes, Route } from 'react-router-dom'
+import './App.css'
+
+function App() {
+  const [count, setCount] = useState(0)
+
+  return (
+    <>
+ <div>
+  <Routes >
+    
+  </Routes>
+ </div>
+    </>
+  )
+}
+
+export default App
---
+++ src/Components/header/HeaderTop.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+function HeaderTop() {
+  return (
+    <div>HeaderTop</div>
+  )
+}
+
+export default HeaderTop
