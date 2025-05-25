7db7ef91363e9c4854777f0142763b486db22ba2
minamirza <minamirzeva@gmail.com>
2025-05-25 22:40:04 +0400

    router

--- src/App.jsx
+++ src/App.jsx
@@ -1,19 +1,26 @@
-import { useState } from 'react'
-import { Routes, Route } from 'react-router-dom'
-import './App.css'
+import { useState } from "react";
+import { Routes, Route, BrowserRouter } from "react-router-dom";
+import "./App.css";
+import Main from "./Components/main/Main";
+import Header from "./Components/header/Header";
+import Footer from "./Components/footer/Footer";
 
 function App() {
-  const [count, setCount] = useState(0)
+  const [count, setCount] = useState(0);
 
   return (
     <>
- <div>
-  <Routes >
-    
-  </Routes>
- </div>
+      <div>
+        <BrowserRouter>
+          <Routes>
+            <Route path="/header" element={<Header />} />
+            <Route path="/" element={<Main />} />
+            <Route path="/footer" element={<Footer />} />
+          </Routes>
+        </BrowserRouter>
+      </div>
     </>
-  )
+  );
 }
 
-export default App
+export default App;
