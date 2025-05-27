88cb8939e26b82e3ee3b31afd25741d94932184d
minamirza <minamirzeva@gmail.com>
2025-05-28 01:21:59 +0400

    sidebar

--- src/Components/main/Main.jsx
+++ src/Components/main/Main.jsx
@@ -1,10 +1,14 @@
 import React from "react";
 import Header from "../header/Header";
+import Sidebar from "./Sidebar";
 
 function Main() {
   return (
     <>
       <Header />
+      <div>
+        <Sidebar />
+      </div>
     </>
   );
 }
