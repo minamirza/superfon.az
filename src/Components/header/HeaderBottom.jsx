4653c9912abf2d75394db5634eb11658c3069e08
minamirza <minamirzeva@gmail.com>
2025-06-18 23:41:02 +0400

    header

--- src/Components/header/HeaderBottom.jsx
+++ src/Components/header/HeaderBottom.jsx
@@ -2,6 +2,10 @@ import React from "react";
 import { Link } from "react-router-dom";
 import "./Header.css";
 import logo from "../../assets/logo.svg";
+import { FaPhoneAlt } from "react-icons/fa";
+import { FaBasketShopping } from "react-icons/fa6";
+import { PiScalesFill } from "react-icons/pi";
+import { GiHearts } from "react-icons/gi";
 
 function HeaderBottom() {
   return (
@@ -9,12 +13,27 @@ function HeaderBottom() {
       <div className="logo">
         <img src={logo} alt="Logo" className="logo" />
       </div>
-      <button type="button" className="w-35 h-11 text-base font-semibold rounded bg-purple-900 text-white">Kataloq</button>
+      <button
+        type="button"
+        className="w-35 h-11 text-base font-semibold rounded bg-purple-900 text-white"
+      >
+        Kataloq
+      </button>
       <input type="text" className="search" placeholder="Axtar..." />
-      <div className="icons">
-        <span>⚖️</span>
-        <span>🤍</span>
-        <span>🛒 0.00 ₼</span>
+      <div className="flex flex-row items-center md:space-x-4 gap-5">
+        <Link to="/muqayise" className="account flex flex-row items-center">
+          <PiScalesFill size={24} />
+        </Link>
+        <Link to="/wishlist" className="account flex flex-row items-center">
+          <GiHearts size={24} />
+        </Link>
+        <Link to="/basket" className="account flex flex-row items-center gap-2">
+          <FaBasketShopping size={24} />
+          <div>
+            <p className="text-[#C1C1C1]">Mebleg</p>
+            <p className="text-[#0a0a47]">0.00</p>
+          </div>
+        </Link>
       </div>
     </div>
   );
--- src/Components/header/HeaderTop.jsx
+++ src/Components/header/HeaderTop.jsx
@@ -1,28 +1,45 @@
 import React from "react";
 import { Link } from "react-router-dom";
-import './Header.css';
+import "./Header.css";
+import { FaPhoneAlt } from "react-icons/fa";
+import { MdPerson } from "react-icons/md";
 
 function HeaderTop() {
   return (
-    <div className="top-bar">
-      <div className="left-links">
-        <Link to="/magazalar">Mağazalarımız</Link>
-        <Link to="/korporativ">Korporativ satış</Link>
+    <header className="p-4 dark:bg-gray-100 dark:text-gray-800">
+      <div className="container flex justify-between h-16 mx-auto">
+        <ul className="flex gap-5 items-stretch hidden space-x-3 lg:flex">
+          <Link to="/magazalar">Mağazalarımız</Link>
+          <Link to="/korporativ">Korporativ satış</Link>
+        </ul>
+        <div className="flex flex-row items-center md:space-x-4">
+          <div className="relative flex flex-row">
+            <Link to="/kampaniyalar">
+              <button className="campaign-btn">Kampaniyalar</button>
+            </Link>
+            <span className="flex items-center gap-2 text-sm text-gray-700">
+              <FaPhoneAlt size={24} />
+              +994 51 205 88 88
+            </span>
+            <span className="flex items-center gap-2 text-sm text-gray-700">
+              <select className="lang-select">
+                <option value="az">AZ</option>
+                <option value="en">EN</option>
+              </select>
+            </span>
+            <span className="flex items-center gap-2 text-sm text-gray-700">
+              <Link to="/hesab" className="account flex flex-row items-center gap-2">
+                <MdPerson size={26} /> 
+                <div>
+                <p className="text-[#C1C1C1]">Daxil ol</p>
+                <p className="text-[#0a0a47]">Hesab</p>
+                </div>
+              </Link>
+            </span>
+          </div>
+        </div>
       </div>
-      <div className="right-info">
-        <Link to="/kampaniyalar">
-          <button className="campaign-btn">Kampaniyalar</button>
-        </Link>
-        <span className="phone">📞 +994 51 205 88 88</span>
-        <select className="lang-select">
-          <option value="az">AZ</option>
-          <option value="en">EN</option>
-        </select>
-        <Link to="/hesab" className="account">
-          👤 Hesab
-        </Link>
-      </div>
-    </div>
+    </header>
   );
 }
 
